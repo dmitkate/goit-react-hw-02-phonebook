e30321c38aec5ad481234754623639d01a61f913
dmitkate <44602492+dmitkate@users.noreply.github.com>
2022-10-27 16:37:29 +0300

    add value

--- src/components/Formin.jsx
+++ src/components/Formin.jsx
@@ -35,6 +35,7 @@ export class FormIn extends Component {
               onChange={this.handleChange}
               type="text"
               name="name"
+              value={this.state.name}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
@@ -46,6 +47,7 @@ export class FormIn extends Component {
               onChange={this.handleChange}
               type="tel"
               name="number"
+              value={this.state.number}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
